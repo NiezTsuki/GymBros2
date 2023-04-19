92cc17462ed3654b39ecbeb9c7649ad73b9c94bb
Daniel.Jofre <da.jofre@duocuc.cl>
2023-04-18 22:49:36 -0400

    Se arregla responsividad y empiezo el carrito

---
+++ carrito.js
@@ -0,0 +1,38 @@
+const productos = [
+    {
+        id: 1,
+        nombre:"PAR MANCUERNAS 5KG",
+        precio: 35990
+    },{
+        id: 2,
+        nombre:"PAR MANCUERNAS 10KG",
+        precio: 59990
+    },{
+        id: 3,
+        nombre:"PAR MANCUERNAS 15KG",
+        precio: 85990
+    },{
+        id: 4,
+        nombre:"PAR MANCUERNAS 20KG",
+        precio: 109990
+    }
+
+];
+
+let contadorCarrito = 0;
+const carrito = [];
+
+const productoCatalogoHTML = (producto) => 
+  {const text = "";
+
+  
+  }
+const productoCarritoHTML = (producto) => "";
+
+const mostrarCatalogo = () => {};
+const mostrarCarrito = () => {};
+
+const botonesCatalogo = () => {};
+const botonesCarrito = () => {};
+
+mostrarCatalogo();
